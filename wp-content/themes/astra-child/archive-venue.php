dad2f4bfde08aea284ff1a954d8aab31b8746212
vijaysingh177 <vijaysingh@protocloudtechnologies.com>
2024-03-04 13:22:57 +0530

    Venue Changes

--- wp-content/themes/astra-child/archive-venue.php
+++ wp-content/themes/astra-child/archive-venue.php
@@ -58,6 +58,7 @@ get_header(); ?>
 
         // The Loop
         if ($query->have_posts()) {
+            $j = 0;
             while ($query->have_posts()) {
                 $query->the_post();
 
@@ -74,7 +75,7 @@ get_header(); ?>
 
 
                 <div class="grid grid-cols-12 lg:mt-16 md:mt-12 mt-10 bg-white rounded-[20px] items-center">
-
+         <?php if($j % 2 == 0){ ?>
                     <div class="lg:col-span-6 col-span-12 lg:order-1 order-2 xl:px-20 md:px-10 px-5 lg:py-0 md:py-10 py-5  lg:rounded-tl-[20px] lg:rounded-bl-[20px] lg:rounded-br-[0px] rounded-bl-[20px] rounded-br-[20px]">
                         <h3 class="lg:text-4xl md:text-3xl text-2xl text-primary bagdoll-display"> <?php echo $title; ?></h3>
                         <p class="xl:text-xl lg:text-lg text-base text-grey mt-3 font-medium border-b border-grey pb-2 xl:mb-7 mb-4">
@@ -89,23 +90,23 @@ get_header(); ?>
                                 $venue_features_details_content = get_sub_field('venue_features_details_content');
                                 if ($i == 0) {
                         ?>
-                                <h4 class="xl:text-xl lg:text-lg text-base text-light-grey font-bold py-1">
-                                    <?php 
-                                        echo $venue_features_details_content;
-                                    } else { ?>
-                                        <h4 class="xl:text-xl lg:text-lg text-base text-light-grey font-bold py-1">   <span class="border-l border-light-grey pl-4 ml-4"><?php echo $venue_features_details_content; ?></span></h4>
+                                    <h4 class="xl:text-xl lg:text-lg text-base text-light-grey font-bold py-1">
+                                    <?php
+                                    echo $venue_features_details_content;
+                                } else { ?>
+                                        <h4 class="xl:text-xl lg:text-lg text-base text-light-grey font-bold py-1"> <span class="border-l border-light-grey pl-4 ml-4"><?php echo $venue_features_details_content; ?></span></h4>
                                     <?php  } ?>
-                                </h4>
-                        <?php
+                                    </h4>
+                            <?php
                                 $i++;
                             endwhile;
                         endif;
-                        ?>
-                        <p class="xl:text-xl lg:text-lg text-base text-grey mt-3 font-medium">
-                            <?php echo $short_description; ?></p>
+                            ?>
+                            <p class="xl:text-xl lg:text-lg text-base text-grey mt-3 font-medium">
+                                <?php echo $short_description; ?></p>
 
-                        <a href="<?php the_permalink(); ?>" class="text-sm text-light-grey font-medium relative z-10 after:content-[''] after:absolute after:-left-2.5 after:top-[50%] after:translate-y-[-50%] after:bg-secondary md:after:h-11 md:after:w-11 after:h-7 after:w-7 after:rounded-full after:z-[-1] block mt-3 ml-3">
-                            EXPLORE THIS VENUE <i class="fa-solid fa-arrow-right ml-2"></i></a>
+                            <a href="<?php the_permalink(); ?>" class="text-sm text-light-grey font-medium relative z-10 after:content-[''] after:absolute after:-left-2.5 after:top-[50%] after:translate-y-[-50%] after:bg-secondary md:after:h-11 md:after:w-11 after:h-7 after:w-7 after:rounded-full after:z-[-1] block mt-3 ml-3">
+                                EXPLORE THIS VENUE <i class="fa-solid fa-arrow-right ml-2"></i></a>
                     </div>
 
                     <div class="lg:col-span-6 col-span-12 lg:order-2 order-1">
@@ -119,6 +120,51 @@ get_header(); ?>
                         <?php } ?>
 
                     </div>
+               <?php }else{ ?>
+                <div class="lg:col-span-6 col-span-12 lg:order-1 order-2">
+                        <?php
+                        // Get the featured image URL
+                        $featured_image_url = get_the_post_thumbnail_url(get_the_ID(), 'thumbnail');
+                        if ($featured_image_url) { ?>
+                            <a href="<?php the_permalink(); ?>">
+                                <img src="<?php echo esc_url($featured_image_url); ?>" width="242" height="317" alt="<?php the_title_attribute(); ?>" class="w-full lg:rounded-tr-[20px] lg:rounded-br-[20px] lg:rounded-tl-[0px] rounded-tl-[20px] rounded-tr-[20px] ">
+                            </a>
+                        <?php } ?>
+
+                    </div>
+                    <div class="lg:col-span-6 col-span-12 lg:order-2 order-1 xl:px-20 md:px-10 px-5 lg:py-0 md:py-10 py-5  lg:rounded-tl-[20px] lg:rounded-bl-[20px] lg:rounded-br-[0px] rounded-bl-[20px] rounded-br-[20px]">
+                        <h3 class="lg:text-4xl md:text-3xl text-2xl text-primary bagdoll-display"> <?php echo $title; ?></h3>
+                        <p class="xl:text-xl lg:text-lg text-base text-grey mt-3 font-medium border-b border-grey pb-2 xl:mb-7 mb-4">
+                            <?php echo $address; ?></p>
+
+
+                        <?php if (have_rows('venue_features_details')) :
+                            $i = 0;
+                            while (have_rows('venue_features_details') && $i < 2) : the_row();
+
+                                // Load sub field value.
+                                $venue_features_details_content = get_sub_field('venue_features_details_content');
+                                if ($i == 0) {
+                        ?>
+                                    <h4 class="xl:text-xl lg:text-lg text-base text-light-grey font-bold py-1">
+                                    <?php
+                                    echo $venue_features_details_content;
+                                } else { ?>
+                                        <h4 class="xl:text-xl lg:text-lg text-base text-light-grey font-bold py-1"> <span class="border-l border-light-grey pl-4 ml-4"><?php echo $venue_features_details_content; ?></span></h4>
+                                    <?php  } ?>
+                                    </h4>
+                            <?php
+                                $i++;
+                            endwhile;
+                        endif;
+                            ?>
+                            <p class="xl:text-xl lg:text-lg text-base text-grey mt-3 font-medium">
+                                <?php echo $short_description; ?></p>
+
+                            <a href="<?php the_permalink(); ?>" class="text-sm text-light-grey font-medium relative z-10 after:content-[''] after:absolute after:-left-2.5 after:top-[50%] after:translate-y-[-50%] after:bg-secondary md:after:h-11 md:after:w-11 after:h-7 after:w-7 after:rounded-full after:z-[-1] block mt-3 ml-3">
+                                EXPLORE THIS VENUE <i class="fa-solid fa-arrow-right ml-2"></i></a>
+                    </div>
+<?php } ?>
 
                 </div>
                 <?php $pagi = pagination_bar($query);
@@ -135,11 +181,11 @@ get_header(); ?>
 
                 ?>
                         <li><a href class="md:text-base text-sm text-white bg-primary rounded-full md:h-10 md:w-10 h-8 w-8 flex items-center justify-center transition">
-                            <?php echo $page_link ?>
+                                <?php echo $page_link ?>
                             </a> </li>
 
                 <?php
-                    endforeach;
+                  endforeach;
                     echo '</ul>';
                 endif;
 
@@ -147,10 +193,10 @@ get_header(); ?>
                 ?>
 
         <?php wp_reset_postdata();
-            }
+           $j++;   }
         }
         ?>
-     
+
 
         <ul class="pagination flex items-center gap-2 justify-center lg:mt-24 md:mt-16 mt-7">
             <li><a href class="md:text-base text-sm text-white bg-primary rounded-full md:h-10 md:w-10 h-8 w-8 flex items-center justify-center transition">1</a></li>
