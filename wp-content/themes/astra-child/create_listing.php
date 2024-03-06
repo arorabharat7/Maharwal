35250f708cf38555f306fdd262f507c6e72cc910
vijaysingh177 <vijaysingh@protocloudtechnologies.com>
2024-03-06 11:33:27 +0530

    new

--- wp-content/themes/astra-child/create_listing.php
+++ wp-content/themes/astra-child/create_listing.php
@@ -11,25 +11,36 @@ get_header(); ?>
     <form id="custom-form" method="post" action="<?php echo home_url(); ?>/new-page/">
         <label for="price-range">Select Price Range:</label>
         <select id="price-range" name="price-range">
-            <option value="1" selected>100-200</option>
-            <option value="1.5">200-300</option>
+        <?php if (have_rows('menu_create_list_option_details')) :
+            while (have_rows('menu_create_list_option_details')) : the_row();
+
+            // Load sub field value.
+            $menu_create_list_option_details_value = get_sub_field('menu_create_list_option_details_value');
+            $menu_create_list_option_details_option_text = get_sub_field('menu_create_list_option_details_option_text');
+           
+
+        ?>
+            <option value="<?php echo $menu_create_list_option_details_value; ?>" >
+            <?php echo $menu_create_list_option_details_option_text; ?></option>
+            <?php endwhile;  endif; ?>
+            <!-- <option value="1.5">200-300</option>
             <option value="2">200-300</option>
             <option value="2.5">200-300</option>
-            <option value="3">200-300</option>
+            <option value="3">200-300</option> -->
             <!-- Add other options here -->
         </select>
 
         <!-- Sample items with prices -->
         <input type="checkbox" id="item1" name="items[]" value="50" data-price="50">
-        <label for="item1">Item 1 - $50</label><br>
+        <label for="item1">Ice Cream  $50</label><br>
         <input type="checkbox" id="item2" name="items[]" value="75" data-price="75">
-        <label for="item2">Item 2 - $75</label><br>
+        <label for="item2">Gulab Jamun  $75</label><br>
         
         <input type="checkbox" id="item3" name="items[]" value="100" data-price="100">
-        <label for="item3">Item 3 - $100</label><br>
+        <label for="item3">Ras malai $100</label><br>
 
         <input type="checkbox" id="item4" name="items[]" value="125" data-price="125">
-        <label for="item4">Item 2 - $125</label><br>
+        <label for="item4">Barfi  $125</label><br>
         <!-- Add more items here -->
 
         <button type="submit">Submit</button>
