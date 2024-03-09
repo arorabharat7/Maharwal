0ba9b0954d5131f35658a495da6c5fd6e42b953b
vijaysingh177 <vijaysingh@protocloudtechnologies.com>
2024-03-09 19:20:15 +0530

    new changes

--- wp-content/themes/astra-child/userswp/bootstrap/reset.php
+++ wp-content/themes/astra-child/userswp/bootstrap/reset.php
@@ -1,91 +1,50 @@
-<?php
+<?php do_action('uwp_template_before', 'reset');
+$css_class = !empty($args['css_class']) ? esc_attr($args['css_class']) : '';
+$form_title = !empty($args['form_title']) ? esc_attr__($args['form_title'], 'userswp') : __('Reset Password', 'userswp');
+?>
+</div>
+</div>
+</div>
+</div>
 
-/**
- * Reset template (default)
- *
- * @ver 1.0.0
- */
-$css_class = !empty($args['css_class']) ? esc_attr($args['css_class']) : 'border-0';
-$form_title = !empty($args['form_title']) || $args['form_title'] == '0' ? esc_attr__($args['form_title'], 'userswp') : __('Reset Password', 'userswp');
-$form_title = apply_filters('uwp_template_form_title', $form_title, 'reset');
-do_action('uwp_template_before', 'reset'); ?>
-
-</div>
-</div>
-</div>
-</div>
-</div>
 <section class="maharwal-login">
 
-	<div class="grid grid-cols-12 ">
+    <div class="uwp-content-wrap <?php echo esc_attr($css_class); ?>">
+        <div class="uwp-login grid grid-cols-12">
 
-		<div class="lg:col-span-5 md:col-span-4 md:block hidden">
-			<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/maharwal_sign-up.webp" alt="login" class="w-full lg:h-[900px] md:h-[780px] object-cover">
-		</div>
-		<div class=" lg:col-span-7 md:col-span-8 col-span-12 ">
-		<div class="text-center xl:py-16 lg:py-12 py-10 ">
-			<div class="card mx-auto container-fluid p-0 <?php echo esc_attr($css_class); ?>">
-				<div class="card-body">
-					<?php
-					// do_action('uwp_template_form_title_before', 'reset');
+            <!-- <div class="uwp-lf-icon"><i class="fas fa-key fa-fw"></i></div> -->
+            <?php do_action('uwp_template_form_title_before', 'reset'); ?>
+            <!-- <h2><?php
+                        // echo apply_filters('uwp_template_form_title', $form_title, 'reset');
+                        ?>
+            </h2> -->
+            <div class="lg:col-span-5 md:col-span-4 md:block hidden">
+                <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/maharwal_login.webp" alt="login" class="w-full h-full object-cover">
+            </div>
+            <div class=" lg:col-span-7 md:col-span-8 col-span-12 ">
+                <div class="text-center xl:py-16 lg:py-12 py-10 ">
+                <a href="#"><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/maharwal_logo.svg" alt="logo" class="m-auto"></a>
 
-					// if ($form_title != '0') {
-					// 	echo '<h3 class="card-title text-center mb-4">';
-					// 	echo esc_attr($form_title);
-					// 	echo '</h3>';
-					// }
-					?>
+<h2 class="lg:text-[42px] md:text-3xl text-2xl font-bold text-dark-grey lg:mt-10 mt-6 lg:mb-4 mb-2">Welcome
+    <span class="text-primary">Back!</span>
+</h2>
+<p class="text-grey md:text-base text-sm ">Let’s Get Started</p>
+                    <?php do_action('uwp_template_display_notices', 'reset'); ?>
+                    <?php if (isset($_GET['key']) && isset($_GET['login'])) { ?>
+                        <form id="loginform" class="uwp-reset-form uwp_form lg:w-[70%] md:w-[80%] w-[90%] m-auto login-form lg:mt-12 mt-8" method="post">
+                            <?php do_action('uwp_template_fields', 'reset'); ?>
+                            <input name="uwp_reset_submit" value="<?php echo __('Submit', 'userswp'); ?>" type="submit"><br>
+                        </form>
+                    <?php } else {
+                        echo sprintf(__('You can not access this page directly. Follow the password reset link you received in your email. To request new password reset link <a href="%s">visit here</a>.', 'userswp'), uwp_get_page_link('forgot'));
+                    } ?>
 
-					<?php do_action('uwp_template_display_notices', 'reset'); ?>
-
-					<?php if (isset($_GET['key']) && isset($_GET['login'])) { ?>
-						<form class="uwp-reset-form uwp_form" method="post">
-							<?php
-							do_action('uwp_template_fields', 'reset');
-							echo aui()->button(array(
-								'type'       => 'submit',
-								'class'      => 'btn btn-primary btn-block text-uppercase',
-								'content'    => __('Submit', 'userswp'),
-								'name'       => 'uwp_reset_submit',
-							));
-							?>
-						</form>
-					<?php } else {
-						echo aui()->alert(array('type' => 'danger', 'content' => sprintf(__('You can not access this page directly. Follow the password reset link you received in your email. To request new password reset link <a href="%s">visit here</a>.', 'userswp'), uwp_get_forgot_page_url())));
-					} ?>
-
-					<div class="uwp-footer-links">
-						<div class="uwp-footer-link float-left">
-							<?php
-							echo aui()->button(array(
-								'type'  =>  'a',
-								'href'       => uwp_get_login_page_url(),
-								'class'      => 'd-block text-center mt-2 small',
-								'content'    => uwp_get_option("login_link_title") ? uwp_get_option("login_link_title") : __('Login', 'userswp'),
-								'extra_attributes'  => array('rel' => 'nofollow')
-							));
-							?>
-						</div>
-						<div class="uwp-footer-link float-right">
-							<?php
-							echo aui()->button(array(
-								'type'  =>  'a',
-								'href'       => uwp_get_register_page_url(),
-								'class'      => 'd-block text-center mt-2 small',
-								'content'    => uwp_get_option("register_link_title") ? uwp_get_option("register_link_title") : __('Create account', 'userswp'),
-								'extra_attributes'  => array('rel' => 'nofollow')
-							));
-							?>
-						</div>
-					</div>
-
-				</div>
-			</div>
-
-		</div>
-		</div>
-	</div>
-
-
+                    <div class="uwp-footer-link uwp-resetpsw"><?php _e('Already a member?', 'userswp'); ?><a rel="nofollow" href="<?php echo uwp_get_login_page_url(); ?>"><?php echo uwp_get_option("login_link_title") ? uwp_get_option("login_link_title") : __('Login here', 'userswp'); ?></a></div>
+                    <div class="clfx"></div>
+                    <div class="uwp-footer-link uwp-register-now"><?php _e('Not a member?', 'userswp'); ?> <a rel="nofollow" href="<?php echo uwp_get_register_page_url(); ?>"><?php echo uwp_get_option("register_link_title") ? uwp_get_option("register_link_title") : __('Create account', 'userswp'); ?></a></div>
+                </div>
+            </div>
+        </div>
+    </div>
 </section>
 <?php do_action('uwp_template_after', 'reset'); ?>
